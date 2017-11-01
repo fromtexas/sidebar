68212c5b3908b8593b24ac69b1d5bbb063097688
fromtexas <telures@yandex.ru>
2017-11-02 00:50:57 +0300

    babel fix

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Sidemenu from './Sidemenu';
 
 
-const Product = ({showFun, showStatus, hideFun, background, icon}) => {
-
-  const renderMenu = () => {
-    if (!showStatus){
-      return;
-    }
-    return <Sidemenu hideMenu={hideFun} />;
-  };
-  return (
-    <li style={{backgroundColor: background}} onClick={showFun} className="product">
-      Продукты
-      <span className="caret">ВСЕ <i className={`fa ${icon}`}></i></span>
-      {renderMenu()}
-    </li>
-  );
-};
+class Product extends Component {
+  render () {
+    const {showFun, showStatus, hideFun, background, icon} = this.props;
+    const renderMenu = () => {
+        if (!showStatus){
+          return;
+        }
+        return <Sidemenu hideMenu={hideFun} />;
+      };
+      return (
+        <li style={{backgroundColor: background}} onClick={showFun} className="product">
+          Продукты
+          <span className="caret">ВСЕ <i className={`fa ${icon}`}></i></span>
+          {renderMenu()}
+        </li>
+      );
+  }
+}
 
 export default Product;
